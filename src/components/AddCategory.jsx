de9a4bff484e2618a48d9e7df62424b2715b5333
XEnryux <djaramm10@gmail.com>
2023-02-21 15:00:48 -0500

    primer commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export const AddCategory = ({onNewCategory}) => {
+
+    const [inputValue, setInputValue ] = useState('');
+
+
+//Esta configuracion de setState para el imput permite que se
+//renderice lo que se va escribiendo 
+//en el teclado mientras se esta escribiendo 
+//dentro de la barra de busqueda
+
+    const onImputChange = ({target}) => {   
+        setInputValue(target.value)
+    };
+
+
+    // funcion ejecutada al enviar el formulario de la barra de busquesa
+    // esta linea de codigo aniade lo escrito en la barra de busqueda al arreglo original
+    // de "categories" de 'GifExpertApp
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if( inputValue.trim().length <= 1) return;
+
+        // setCategories(categories => [inputValue,...categories]);
+        onNewCategory(inputValue.trim());
+        setInputValue('');
+      
+
+    }
+
+    return (
+        <form onSubmit= { onSubmit }>
+            <input
+                type="text"
+                placeholder="Buscar Gifs"
+                value={inputValue}
+                onChange={onImputChange}
+            />
+        </form>
+    )
+}
